cd7d2e07cc03945357f32a8e29c00b9e5a7eb1d1
Thomas Collignon <t-collignon@efluid.fr>
2018-02-23 14:02:54 +0100

    Ajout d'un belle javadoc

--- src/main/java/org/junit/Assert.java
+++ src/main/java/org/junit/Assert.java
@@ -121,6 +121,12 @@ public class Assert {
         }
     }
 
+    /**
+     * Beautiful javadoc
+     * @param expected
+     * @param actual
+     * @return
+     */
     private static boolean equalsRegardingNull(Object expected, Object actual) {
         if (expected == null) {
             return actual == null;
