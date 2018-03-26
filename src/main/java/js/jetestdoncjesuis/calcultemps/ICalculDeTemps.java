04f192acfb2fe14e7941f98bb6c29d2325c4ec1b
Jerome Tamborini <j-tamborini@efluid.fr>
2018-03-26 17:10:06 +0200

    premier commit pour calcul (interfaces).

---
+++ src/main/java/js/jetestdoncjesuis/calcultemps/ICalculDeTemps.java
@@ -0,0 +1,5 @@
+package js.jetestdoncjesuis.calcultemps;
+
+public interface ICalculDeTemps {
+
+}
---
+++ src/main/java/js/jetestdoncjesuis/calcultemps/ICalculateurDeTempsDexecutionService.java
@@ -0,0 +1,18 @@
+package js.jetestdoncjesuis.calcultemps;
+
+/**
+ * Définit les services de calcul de temps d'exéction.
+ */
+public interface ICalculateurDeTempsDexecutionService {
+
+  /**
+   * Initie le calcul de temps pour une méthode de test.
+   */
+  public void lancerCalculTempsMethode(ICalculDeTemps calculDeTemps);
+
+  /**
+   * Stoppe le calcul de temps d'une méthode.
+   */
+  public void arreterCalculTempsMethode(ICalculDeTemps calculDeTemps);
+
+}
